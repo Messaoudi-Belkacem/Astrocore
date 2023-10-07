6f2729e31279abe2f67e96d07513078bb682897b
Abdelhai1 <112305874+Abdelhai1@users.noreply.github.com>
2023-10-07 07:07:43 +0100

    backend and profile

--- app/src/main/java/com/example/astrocore/activities/ProfileFragment.kt
+++ app/src/main/java/com/example/astrocore/activities/ProfileFragment.kt
@@ -1,14 +1,27 @@
 package com.example.astrocore.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.astrocore.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlin.math.log
 
 class ProfileFragment : Fragment() {
+    private var auth : FirebaseAuth = FirebaseAuth.getInstance()
+    private var db : FirebaseFirestore = FirebaseFirestore.getInstance()
+    private lateinit var logoutBt :Button
+    private lateinit var tname : TextView
+    private lateinit var tuserName : TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,6 +29,23 @@ class ProfileFragment : Fragment() {
     ): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_home, container, false)
         Log.d("AboutMovieFragment.java", "The onCreateView method called")
+        logoutBt = rootView.findViewById(R.id.button4)
+        tname = rootView.findViewById(R.id.textView4)
+        tuserName = rootView.findViewById(R.id.textView9)
+
+        val id =auth.currentUser!!.uid
+        db.collection("users").document(id).get().addOnCompleteListener {
+            val name = it.result!!.data?.getValue("name").toString().trim()
+            val uname = it.result!!.data?.getValue("username").toString().trim()
+        }
+        logoutBt.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(this.context,WelcomeActivity::class.java)
+            intent.putExtra("h","hh")
+            startActivity(intent)
+        }
         return rootView
+
+
     }
 }
